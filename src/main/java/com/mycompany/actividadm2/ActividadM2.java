bafbb3b83c44711cebf175fd051d21069ccdb82f
Nicolas Morales <nicolasmoralesj03@gmail.com>
2023-10-26 19:32:59 -0300

    firts commit

---
+++ src/main/java/com/mycompany/actividadm2/ActividadM2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.actividadm2;
+
+import com.mycompany.actividadm2.vista.VentanaPrincipal;
+
+/**
+ *
+ * @author Nico Morales
+ */
+public class ActividadM2 {
+
+    public static void main(String[] args) {
+      
+        VentanaPrincipal ventana = new VentanaPrincipal();
+        ventana.setVisible(true);
+        ventana.setLocationRelativeTo(null);
+    }
+}
