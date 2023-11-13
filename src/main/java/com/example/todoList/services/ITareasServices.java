2a2e0ede78ab74c96e66ded5ec33c70a22aceddf
Nicolas Morales <nicolasmoralesj03@gmail.com>
2023-11-13 01:19:00 -0300

    se termina de crear todos los endpoints y se termina el service

--- src/main/java/com/example/todoList/services/ITareasServices.java
+++ src/main/java/com/example/todoList/services/ITareasServices.java
@@ -1,11 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
+
 package com.example.todoList.services;
 
+import com.example.todoList.model.Message;
 import com.example.todoList.model.Tareas;
-import org.springframework.data.domain.Pageable;
+import java.util.List;
+import java.util.Optional;
 
 /**
  *
@@ -13,9 +12,9 @@ import org.springframework.data.domain.Pageable;
  */
 public interface ITareasServices {
     
-    public Pageable traerTareas();
-    public Tareas traerTarea(Long id);
-    public String crearTareas(Tareas tarea);
-    public String borrarTarea(Long id);
-    public String editarTarea(Tareas tarea);
+    public List<Tareas> traerTareas();
+    public Optional<Tareas> traerTarea(Long id);
+    public Message crearTareas(Tareas tarea);
+    public Message borrarTarea(Long id);
+    public Message editarTarea(Tareas tarea);
 }
