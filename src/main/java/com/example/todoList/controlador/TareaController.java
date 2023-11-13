1a5c8baf0dd673f6f633c8c82743419df042c802
Nicolas Morales <nicolasmoralesj03@gmail.com>
2023-11-13 00:43:14 -0300

    se agrega swagger

--- src/main/java/com/example/todoList/controlador/TareaController.java
+++ src/main/java/com/example/todoList/controlador/TareaController.java
@@ -1,19 +1,23 @@
 
 package com.example.todoList.controlador;
 
+import com.example.todoList.model.Tareas;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
  * @author Nico Morales
  */
 
+@RestController
 @RequestMapping("/tareas")
 public class TareaController {
     
@@ -23,14 +27,14 @@ public class TareaController {
         return new ResponseEntity("Traigo", HttpStatus.ACCEPTED);
     }
     
-    @GetMapping("/traer/{id]")
+    @GetMapping("/traer/{id}")
     public ResponseEntity<?> traerTarea(@PathVariable Long id){
         return new ResponseEntity("Traigo 1", HttpStatus.ACCEPTED);
     }
     
     
     @PostMapping("/creo")
-    public ResponseEntity<?> crearTarea(){
+    public ResponseEntity<?> crearTarea(@RequestBody Tareas tarea){
         return new ResponseEntity("Creo", HttpStatus.ACCEPTED);
     }
     
