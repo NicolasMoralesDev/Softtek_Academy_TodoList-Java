c8d99a9a6dfc462d7ae4063a5bc39f9066d0ab87
Nicolas Morales <nicolasmoralesj03@gmail.com>
2023-11-14 18:18:21 -0300

    se configura cors

--- src/main/java/com/example/todoList/controlador/TareaController.java
+++ src/main/java/com/example/todoList/controlador/TareaController.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RequestMapping("/tareas")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class TareaController {
  
