1f368b7b2a3457d4333d81c7dac47188351dcc55
Nicolas Morales <nicolasmoralesj03@gmail.com>
2023-11-16 17:48:50 -0300

    se agrega favicon

--- src/context/StateProvider.tsx
+++ src/context/StateProvider.tsx
@@ -1,23 +1,23 @@
 import { open } from "@material-tailwind/react/types/components/accordion";
 import { StateContext } from "./StateContext"
-import {useState} from "react"
+import { useState } from "react"
 
-interface props{
+interface props {
   children: JSX.Element | JSX.Element[]
 }
 
-export const StateProvider = ({children}: props) => {
+export const StateProvider = ({ children }: props) => {
 
-const [modal, setModal] = useState <boolean>(false);
-const [flag, setFlag] = useState <boolean>(false);
-const [edit, setEdit] = useState <any>([]);
+  const [modal, setModal] = useState<boolean>(false);
+  const [flag, setFlag] = useState<boolean>(false);
+  const [edit, setEdit] = useState<any>([]);
 
 
 
 
   return (
- 
-    <StateContext.Provider value={{modal, setModal, flag, setFlag, edit, setEdit}}>
+
+    <StateContext.Provider value={{ modal, setModal, flag, setFlag, edit, setEdit }}>
       {children}
     </StateContext.Provider>
   )
